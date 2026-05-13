bd9a35d4da1b3d6162c8d1d75406006c390ec6ff
Subash B <subash59245@gmail.com>
2026-05-13 14:01:13 +0530

    Completed Navbar and initialised all containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Hero from './containers/hero/Hero';
+import About from './containers/about/About';
+import Skills from './containers/skills/Skills';
+import Projects from './containers/projects/Projects';
+import Contact from './containers/contact/Contact';
 
 const App = () => {
   return (
-    <>
-    </>
+    <div className='App'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
   )
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/logo.png'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <img 
+            src={logo} 
+            alt="logo" 
+            className='navbar-logo' 
+        />
+        <ul className="navbar-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
